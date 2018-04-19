f77547cdb84c647cb59579bf319d67eea7e331a0
jitendra <jitendra.auxesis@outlook.com>
2018-04-19 15:16:57 +0530

    ASDF

--- test_project/urls.py
+++ test_project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from myapp import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index,name='index'),
+	url(r'^myapp/',include('myapp.urls')),
 ]
