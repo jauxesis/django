3b1248784db4918df95fcf6af943b7f32e6b9972
Ubuntu <ubuntu@ip-192-168-1-134.ap-south-1.compute.internal>
2018-04-19 08:22:30 +0000

    afd

--- test_project/settings.py
+++ test_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')0)v7+md5p^5m_9db-hf&2vq9y6rdvj8kon2igwx4-5%jhreo&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
