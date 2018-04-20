ff7527c365b70dbf33da0c0fb1b9efd6acdfca0f
jitendra <jitendra.auxesis@outlook.com>
2018-04-20 13:03:10 +0530

    Sdf

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.home,name='home'),
+    url(r'^$',views.index,name='index'),
     url(r'^home',views.home,name='home'),
+    url(r'^welcome',views.welcome,name='welcome'),
+    url(r'^s3',views.uploads3,name='s3'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,10 +2,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 # Create your views here.
+
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render_to_response
+
 from django.template import Context, loader
 
 import datetime
@@ -18,3 +21,10 @@ def index(request):
 def home(request):
 	# return render_to_response("myapp/templates/test.html")
 	return render(request, 'test.html')
+
+def welcome(request):
+	html = "<html><body>Its Welcome Page</body></html>"
+	return HttpResponse(html)
+
+def uploads3(request):
+	return render(request,'s3.html',{"formTitle":"AWS S3 Bucket File Uplaod"})
